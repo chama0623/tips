5e6719b704d67515fe94160b50be385cdba4827b
yudai0731 <yudai40018@gmail.com>
2021-04-13 19:32:38 +0900

    add sample of function description

---
+++ python/function_description_sample.py
@@ -0,0 +1,14 @@
+# sample of function description
+
+def func(args):
+    """description of function
+    
+    Args:
+        args : description
+
+    Returns:
+        return : description
+    """
+
+# 関数の説明を表示したいとき
+print(func.__doc__)
